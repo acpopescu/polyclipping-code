c02dd7823d36cc568e000beb2e2bd16779b3da91
angusj <angusj@758e7f8d-410f-429b-b4f6-457a56196ea1>
2022-02-24 01:34:42 +0000

    Added credits WRT C# port
    git-svn-id: svn://svn.code.sf.net/p/polyclipping/code/trunk@582 758e7f8d-410f-429b-b4f6-457a56196ea1

--- Clipper2 beta/C#/Clipper.cs
+++ Clipper2 beta/C#/Clipper.cs
@@ -7,8 +7,12 @@
 * Purpose   :  This module contains simple functions that will likely cover    *
 *              most polygon boolean and offsetting needs, while also avoiding  *
 *              the complexities of the other modules in this clipping library. *
-* License   :  http://www.boost.org/LICENSE_1_0.txt                            *
+*Thanks     :  Special thanks to Thong Nguyen (https://nguyen.mn/) and to      *
+*              Guus Kuiper (https://www.guuskuiper.nl/) for their invaluable   *
+*              assistance in this C# Clipper port.                             *
+*License   :  http://www.boost.org/LICENSE_1_0.txt                             *
 *******************************************************************************/
+
 
 using System.Collections.Generic;
 
